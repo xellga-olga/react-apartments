e7c6562ce647901141d3097b67f103f48b3f74dd
Noskina Olha <noskinaolga1@gmail.com>
2024-12-07 23:57:12 +0100

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
 import "./layout.scss";
+
 function App() {
-  return <div className="layout">Hello</div>;
+  return (
+    <div className="layout">
+      <BrowserRouter>
+        <Navbar />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import "./Navbar.scss";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  const [open, setOpen] = useState(false);
+
+  const user = true;
+  return (
+    <nav>
+      <div className="left">
+        <a href="/" className="logo">
+          <img src="/logo.png" alt="logo" />
+          <span>React-Apartments</span>
+        </a>
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Contact</a>
+        <a href="/">Agents</a>
+      </div>
+
+      <div className="right">
+        {user ? (
+          <div className="user">
+            <img src="/user.png" alt="" />
+            <span>John Doe</span>
+            <Link to="/profile" className="profile">
+              <div className="notification">3</div>
+              <span>Profile</span>
+            </Link>
+          </div>
+        ) : (
+          <>
+            <a href="/">Sign in</a>
+            <a href="/" className="register">
+              Sign up
+            </a>
+          </>
+        )}
+
+        <div className="menuIcon">
+          <img
+            src="/menu.png"
+            alt=""
+            onClick={() => setOpen((prev) => !prev)}
+          />
+        </div>
+
+        <div className={open ? "menu active" : "menu"}>
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+          <a href="/">Agents</a>
+          <a href="/">Sign in</a>
+          <a href="/">Sign up</a>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
