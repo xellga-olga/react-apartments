a7fa87d91e27f2dcf7e9936a4d0c61d84ec5a777
Noskina Olha <noskinaolga1@gmail.com>
2024-12-13 21:51:43 +0100

    Implemented the Map component using react-leaflet and leaflet

--- src/routes/listPage/listPage.jsx
+++ src/routes/listPage/listPage.jsx
@@ -1,25 +1,26 @@
 import Card from "../../components/card/Card";
 import Filter from "../../components/filter/Filter";
+import Map from "../../components/map/Map";
 import { listData } from "../../data/data";
 import "./listPage.scss";
 
 function ListPage() {
-   const data = listData
-
+  const data = listData;
 
   return (
     <div className="listPage">
       <div className="listContainer">
         <div className="wrapper">
-         <Filter />
-         {
-            data.map(item => (
-               <Card key={item.id} item={item} />
-            ))
-         }
+          <Filter />
+          {data.map((item) => (
+            <Card key={item.id} item={item} />
+          ))}
         </div>
       </div>
-      <div className="mapContainer">map</div>
+
+      <div className="mapContainer">
+        <Map items={data}/>
+      </div>
     </div>
   );
 }
